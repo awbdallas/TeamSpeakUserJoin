3f8ca2b0e25e5f87534c65e9d193f2cac7ea9122
Austin <awbdallas@gmail.com>
2016-09-18 19:06:37 -0500

    - created send_message - added leaving message

--- TeamSpeakUserJoin.php
+++ TeamSpeakUserJoin.php
@@ -26,11 +26,10 @@
  * Check for certain options
  *
 */ 
-/*-------SETTINGS-------*/
 
+/*-------SETTINGS-------*/
 $ini_array = parse_ini_file("config.ini");
 
-//Wanted to put them in a file so I don't commit with passwords here
 $ts3_ip = $ini_array['ts3_ip'];
 $ts3_queryport = $ini_array['ts3_queryport'];
 $ts3_user = $ini_array['ts3_user'];
@@ -40,6 +39,7 @@ $library_path = $ini_array['library_path'];
 $time_for_checking = $ini_array['time_for_checking'];
 $post_url = $ini_array['post_url'];
 $slack_channel = $ini_array['slack_channel'];
+$users_file = $ini_array['users_file'];
 /*----------------------*/
 
 #Include ts3admin.class.php
@@ -47,65 +47,114 @@ require("$library_path");
 #build a new ts3admin object
 $tsAdmin = new ts3admin($ts3_ip, $ts3_queryport);
 
+# Current Users
+$new_users = array();
+$left_users = array();
+
+# Adding users that we got from last query, oh and is everything in PHP a 
+# hack?
+$previous_users  = file($users_file, 
+	FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+
 if($tsAdmin->getElement('success', $tsAdmin->connect())) {
-	#login as serveradmin
+
+	# login as serveradmin
 	$tsAdmin->login($ts3_user, $ts3_pass);
-	
-	#select teamspeakserver
+	# select teamspeakserver
 	$tsAdmin->selectServer($ts3_port);
-	
-	#get clientlist
+	# get clientlist
 	$clients = $tsAdmin->clientList();
+	
+	$current_users_file = fopen($users_file, "w") or die ("Unable to open!");
 
 	foreach($clients['data'] as $client){
 		$userinfo = $tsAdmin->clientInfo($client['clid'])['data'];
-		// In seconds
-		$time_since_login = $userinfo['connection_connected_time'] / 1000;
+		// Seconds are nicer to work with
+		$user_name = $userinfo['client_nickname'];
 		
 		// Trying only to get users
-		if ($userinfo['client_version'] == 'ServerQuery'){
+		if ($userinfo['client_version'] == 'ServerQuery' ||	$user_name == ""){	
 			continue;
 		}
 
-		if ($time_since_login < $time_for_checking){
-			// Taken from this thread: http://stackoverflow.com/questions/6213509/send-json-post-using-php
-			$user_name = $userinfo['client_nickname'];
-			if ($user_name == ""){
-				continue;
-			}
-			$message = "" . $user_name . " has joined the TeamSpeak server";
-			$data = array(
-				'channel'	=> $slack_channel,
-				'username'	=> 'webhookbot',
-				'text'		=> $message,
-				"icon_emoji"=> ":ghost:"
-			);
+		fwrite($current_users_file, "$user_name\n");
 
-			$content = json_encode ( $data );
-			$curl = curl_init($post_url);
-			curl_setopt($curl, CURLOPT_HEADER, false);
-			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($curl, CURLOPT_HTTPHEADER,
-					array("Content-type: application/json"));
-			curl_setopt($curl, CURLOPT_POST, true);
-			curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
+		if(in_array($user_name, $previous_users)){
+			# Don't have to worry about it now
+			unset($previous_users, $user_name);
+		}else{
+			# New User
+			$new_users[$user_name] = "";
+		}
 
-			$json_response = curl_exec($curl);
-
-		}	
-	} 
+	}
 	$tsAdmin->logout();
+	
 }else{
 	echo 'Connection could not be established.';
 }
 
+# Someone left : ( 
+if (!empty($previous_users)){
+	// Note: I do this just to try to make it a little nicer on the names 
+	$left_users = $previous_users;		
+}
+
+
+if (!empty($new_users)){
+	// Normal Message
+	$message = "";
+
+	foreach($new_users as $key => $value){
+		$message += "$key has joined the teamspeak\n";
+	}
+
+	send_message($message, $slack_channel, $post_url);
+}
+
+if (!empty($left_users)){
+	$message = "";
+
+	foreach($new_users as $key => $value){
+		$message += "$key has left the teamspeak\n";
+	}
+	
+	send_message($message, $slack_channel, $post_url);
+}
 /**
  * This code retuns all errors from the debugLog
  */
 
 if(count($tsAdmin->getDebugLog()) > 0) {
 	foreach($tsAdmin->getDebugLog() as $logEntry) {
-		echo '<script>alert("'.$logEntry.'");</script>';
+		print($logEntry);
 	}
+}
+
+
+# I think it's a different namespace? But who knows cause php
+function send_message($message, $slack_channel, $post_url){
+// Taken from this thread: 
+// http://stackoverflow.com/questions/6213509/send-json-post-using-php
+	print("here");	
+	$data = array(
+		'channel'	=> $slack_channel,
+		'username'	=> 'webhookbot',
+		'text'		=> $message,
+		"icon_emoji"=> ":ghost:"
+	);
+
+	$content = json_encode ( $data );
+	$curl = curl_init($post_url);
+	curl_setopt($curl, CURLOPT_HEADER, false);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($curl, CURLOPT_HTTPHEADER,
+			array("Content-type: application/json"));
+	curl_setopt($curl, CURLOPT_POST, true);
+	curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
+
+	$json_response = curl_exec($curl);
+
 }
 ?>
